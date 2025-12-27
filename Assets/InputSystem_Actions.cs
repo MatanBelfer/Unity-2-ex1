4f6197259d7397f98f8f28cfe80ef0b4d215a69e
MatanBelfer <belfer.matan@gmail.com>
2025-12-27 14:31:13 +0200

    added select dest at right click
    added:
    1.  set destination of agent to right click
    2. center  at agent with space bar
    3. change agent with 1 and 2
    
    TODO:
    1. fix zoom
    2.animations

--- Assets/InputSystem_Actions.cs
+++ Assets/InputSystem_Actions.cs
@@ -114,7 +114,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""name"": ""Attack"",
                     ""type"": ""Button"",
                     ""id"": ""6c2ab1b8-8984-453a-af3d-a3c78ae1679a"",
-                    ""expectedControlType"": ""Button"",
+                    ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
@@ -123,7 +123,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""name"": ""Interact"",
                     ""type"": ""Button"",
                     ""id"": ""852140f2-7766-474d-8707-702459ba45f3"",
-                    ""expectedControlType"": ""Button"",
+                    ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": ""Hold"",
                     ""initialStateCheck"": false
@@ -150,7 +150,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""name"": ""Previous"",
                     ""type"": ""Button"",
                     ""id"": ""2776c80d-3c14-4091-8c56-d04ced07a2b0"",
-                    ""expectedControlType"": ""Button"",
+                    ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
@@ -159,7 +159,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""name"": ""Next"",
                     ""type"": ""Button"",
                     ""id"": ""b7230bb6-fc9b-4f52-8b25-f5e19cb2c2ba"",
-                    ""expectedControlType"": ""Button"",
+                    ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
@@ -168,7 +168,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""name"": ""Sprint"",
                     ""type"": ""Button"",
                     ""id"": ""641cd816-40e6-41b4-8c3d-04687c349290"",
-                    ""expectedControlType"": ""Button"",
+                    ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
@@ -186,6 +186,33 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""name"": ""Recenter"",
                     ""type"": ""Button"",
                     ""id"": ""6db146d0-74a0-4d22-a4cc-afc25aed12ae"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""RightClick"",
+                    ""type"": ""Button"",
+                    ""id"": ""374c4587-6682-466f-a8dd-71c37c1f5dd7"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Agent1"",
+                    ""type"": ""Button"",
+                    ""id"": ""2d71ae77-3c2c-4d50-ac3c-45a22855e57e"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Agent2"",
+                    ""type"": ""Button"",
+                    ""id"": ""4001b24d-3b46-463a-bbe8-85eec70027e1"",
                     ""expectedControlType"": """",
                     ""processors"": """",
                     ""interactions"": """",
@@ -597,6 +624,39 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""groups"": """",
                     ""action"": ""Recenter"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""9510ae4f-70af-4bf3-a6c5-bfebd5d0eec3"",
+                    ""path"": ""<Mouse>/rightButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""RightClick"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""4009d42c-c3c1-45b2-beb7-8f32ccd5f3b2"",
+                    ""path"": ""<Keyboard>/1"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Agent1"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""4c899b77-9ec1-4122-8f57-fda748ffeda8"",
+                    ""path"": ""<Keyboard>/2"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Agent2"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -1195,6 +1255,9 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         m_Player_Sprint = m_Player.FindAction("Sprint", throwIfNotFound: true);
         m_Player_Zoom = m_Player.FindAction("Zoom", throwIfNotFound: true);
         m_Player_Recenter = m_Player.FindAction("Recenter", throwIfNotFound: true);
+        m_Player_RightClick = m_Player.FindAction("RightClick", throwIfNotFound: true);
+        m_Player_Agent1 = m_Player.FindAction("Agent1", throwIfNotFound: true);
+        m_Player_Agent2 = m_Player.FindAction("Agent2", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -1299,6 +1362,9 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Sprint;
     private readonly InputAction m_Player_Zoom;
     private readonly InputAction m_Player_Recenter;
+    private readonly InputAction m_Player_RightClick;
+    private readonly InputAction m_Player_Agent1;
+    private readonly InputAction m_Player_Agent2;
     /// <summary>
     /// Provides access to input actions defined in input action map "Player".
     /// </summary>
@@ -1354,6 +1420,18 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         /// Provides access to the underlying input action "Player/Recenter".
         /// </summary>
         public InputAction @Recenter => m_Wrapper.m_Player_Recenter;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/RightClick".
+        /// </summary>
+        public InputAction @RightClick => m_Wrapper.m_Player_RightClick;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/Agent1".
+        /// </summary>
+        public InputAction @Agent1 => m_Wrapper.m_Player_Agent1;
+        /// <summary>
+        /// Provides access to the underlying input action "Player/Agent2".
+        /// </summary>
+        public InputAction @Agent2 => m_Wrapper.m_Player_Agent2;
         /// <summary>
         /// Provides access to the underlying input action map instance.
         /// </summary>
@@ -1413,6 +1491,15 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
             @Recenter.started += instance.OnRecenter;
             @Recenter.performed += instance.OnRecenter;
             @Recenter.canceled += instance.OnRecenter;
+            @RightClick.started += instance.OnRightClick;
+            @RightClick.performed += instance.OnRightClick;
+            @RightClick.canceled += instance.OnRightClick;
+            @Agent1.started += instance.OnAgent1;
+            @Agent1.performed += instance.OnAgent1;
+            @Agent1.canceled += instance.OnAgent1;
+            @Agent2.started += instance.OnAgent2;
+            @Agent2.performed += instance.OnAgent2;
+            @Agent2.canceled += instance.OnAgent2;
         }
 
         /// <summary>
@@ -1457,6 +1544,15 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
             @Recenter.started -= instance.OnRecenter;
             @Recenter.performed -= instance.OnRecenter;
             @Recenter.canceled -= instance.OnRecenter;
+            @RightClick.started -= instance.OnRightClick;
+            @RightClick.performed -= instance.OnRightClick;
+            @RightClick.canceled -= instance.OnRightClick;
+            @Agent1.started -= instance.OnAgent1;
+            @Agent1.performed -= instance.OnAgent1;
+            @Agent1.canceled -= instance.OnAgent1;
+            @Agent2.started -= instance.OnAgent2;
+            @Agent2.performed -= instance.OnAgent2;
+            @Agent2.canceled -= instance.OnAgent2;
         }
 
         /// <summary>
@@ -1834,6 +1930,27 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
         /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
         void OnRecenter(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "RightClick" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnRightClick(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Agent1" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnAgent1(InputAction.CallbackContext context);
+        /// <summary>
+        /// Method invoked when associated input action "Agent2" is either <see cref="UnityEngine.InputSystem.InputAction.started" />, <see cref="UnityEngine.InputSystem.InputAction.performed" /> or <see cref="UnityEngine.InputSystem.InputAction.canceled" />.
+        /// </summary>
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.started" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.performed" />
+        /// <seealso cref="UnityEngine.InputSystem.InputAction.canceled" />
+        void OnAgent2(InputAction.CallbackContext context);
     }
     /// <summary>
     /// Interface to implement callback methods for all input action callbacks associated with input actions defined by "UI" which allows adding and removing callbacks.
--- Assets/Scripts/RTSNorthUpCam.cs
+++ Assets/Scripts/RTSNorthUpCam.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Unity.Cinemachine;
+using UnityEngine.AI;
 
 public class RTSNorthUpCam : MonoBehaviour
 {
@@ -10,11 +11,13 @@ public class RTSNorthUpCam : MonoBehaviour
     public InputActionReference move; // Vector2 (WASD)
 
     public InputActionReference zoom; // Axis (Mouse scroll y)
-
     public InputActionReference reCenter; // space 
+    public InputActionReference rightClick;
+    public InputActionReference selectAgent1;
+    public InputActionReference selectAgent2;
 
-    [Header("Tuning")] public float moveSpeed = 20f; // units/second
-    public float zoomSpeed = 8f; // feel-tuning
+    [Header("Tuning")] public float moveSpeed = 20f;
+    public float zoomSpeed = 8f;
     public float minDist = 10f;
     public float maxDist = 60f;
 
@@ -29,8 +32,19 @@ public class RTSNorthUpCam : MonoBehaviour
     public AgentType selectedAgentType;
     [SerializeField] private GameObject cameraTargetAgent1;
     [SerializeField] private GameObject cameraTargetAgent2;
-    private GameObject selectedAgentTarget;
+    private GameObject selectedAgentCameraTarget;
 
+    [Header("Nav Mesh Movement")] [SerializeField]
+    private NavMeshAgent agent1;
+
+    [SerializeField] private NavMeshAgent agent2;
+    private NavMeshAgent selectedAgentNav;
+
+    [SerializeField] private Camera mainCamera; // Reference to the main camera for raycasting
+    [SerializeField] private LayerMask groundLayerMask = 1; // LayerMask for the ground/terrain layer
+
+    private bool isRightClickHeld = false;
+    private bool isSpaceHeld = false;
 
     void Awake()
     {
@@ -38,38 +52,97 @@ public class RTSNorthUpCam : MonoBehaviour
             follow = vcam.GetComponent<CinemachineFollow>();
         else
             Debug.LogError("RTSNorthUpCam: No Cinemachine Camera found");
-
-        
     }
 
     void OnEnable()
     {
-        move?.action.Enable();
-        zoom?.action.Enable();
-        reCenter?.action.Enable();
+        if (move != null)
+            move.action.Enable();
+        
+        if (zoom != null)
+            zoom.action.Enable();
+        
+        if (reCenter != null)
+        {
+            reCenter.action.Enable();
+            reCenter.action.started += OnReCenterStarted;
+            reCenter.action.canceled += OnReCenterCanceled;
+        }
+        
+        if (rightClick != null)
+        {
+            rightClick.action.Enable();
+            rightClick.action.started += OnRightClickStarted;
+            rightClick.action.canceled += OnRightClickCanceled;
+        }
+        
+        if (selectAgent1 != null)
+        {
+            selectAgent1.action.Enable();
+            selectAgent1.action.performed += OnSelectAgent1;
+        }
+        
+        if (selectAgent2 != null)
+        {
+            selectAgent2.action.Enable();
+            selectAgent2.action.performed += OnSelectAgent2;
+        }
     }
 
     void OnDisable()
     {
-        move?.action.Disable();
-        zoom?.action.Disable();
-        reCenter?.action.Enable();
+        if (move != null)
+            move.action.Disable();
+        
+        if (zoom != null)
+            zoom.action.Disable();
+        
+        if (reCenter != null)
+        {
+            reCenter.action.started -= OnReCenterStarted;
+            reCenter.action.canceled -= OnReCenterCanceled;
+            reCenter.action.Disable();
+        }
+        
+        if (rightClick != null)
+        {
+            rightClick.action.started -= OnRightClickStarted;
+            rightClick.action.canceled -= OnRightClickCanceled;
+            rightClick.action.Disable();
+        }
+        
+        if (selectAgent1 != null)
+        {
+            selectAgent1.action.performed -= OnSelectAgent1;
+            selectAgent1.action.Disable();
+        }
+        
+        if (selectAgent2 != null)
+        {
+            selectAgent2.action.performed -= OnSelectAgent2;
+            selectAgent2.action.Disable();
+        }
     }
 
     void Update()
     {
-        selectedAgentTarget = selectedAgentType == AgentType.Agent1 ? cameraTargetAgent1 : cameraTargetAgent2;
-        if (reCenter != null && reCenter.action.triggered)
+        selectedAgentCameraTarget = selectedAgentType == AgentType.Agent1 ? cameraTargetAgent1 : cameraTargetAgent2;
+        selectedAgentNav = selectedAgentType == AgentType.Agent1 ? agent1 : agent2;
+        
+        if (isRightClickHeld)
         {
-            transform.position = selectedAgentTarget.transform.position;
+            SetNavMeshDestination();
         }
-
-        // Pan in WORLD space (north up)
+        
+        if (isSpaceHeld && selectedAgentCameraTarget != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, selectedAgentCameraTarget.transform.position, Time.deltaTime * 5f); // Smooth following
+        }
+        
         Vector2 m = move != null ? move.action.ReadValue<Vector2>() : Vector2.zero;
         Vector3 delta = new Vector3(m.x, 0f, m.y) * (moveSpeed * Time.deltaTime);
         transform.position += delta;
 
-        // Zoom by changing FollowOffset distance (keeps same angle)
         if (follow != null && zoom != null)
         {
             float s = zoom.action.ReadValue<float>();
@@ -81,4 +154,80 @@ public class RTSNorthUpCam : MonoBehaviour
             }
         }
     }
+
+    private void OnRightClickStarted(InputAction.CallbackContext context)
+    {
+        isRightClickHeld = true;
+    }
+
+    private void OnRightClickCanceled(InputAction.CallbackContext context)
+    {
+        isRightClickHeld = false;
+    }
+
+    private void OnReCenterStarted(InputAction.CallbackContext context)
+    {
+        isSpaceHeld = true;
+    }
+
+    private void OnReCenterCanceled(InputAction.CallbackContext context)
+    {
+        isSpaceHeld = false;
+    }
+
+    private void OnSelectAgent1(InputAction.CallbackContext context)
+    {
+        SelectAgent(AgentType.Agent1);
+    }
+
+    private void OnSelectAgent2(InputAction.CallbackContext context)
+    {
+        SelectAgent(AgentType.Agent2);
+    }
+
+    private void SelectAgent(AgentType agentType)
+    {
+        if (selectedAgentType != agentType)
+        {
+            selectedAgentType = agentType;
+            Debug.Log($"Agent {agentType} selected");
+        }
+    }
+
+    void SetNavMeshDestination()
+    {
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+
+        Camera cameraToUse = mainCamera;
+        if (cameraToUse == null)
+        {
+            cameraToUse = Camera.main; // Fallback to main camera
+        }
+
+        if (cameraToUse == null)
+        {
+            Debug.LogWarning("No camera found for raycasting!");
+            return;
+        }
+
+        Ray ray = cameraToUse.ScreenPointToRay(mousePosition);
+
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, groundLayerMask))
+        {
+            if (selectedAgentNav != null)
+            {
+                selectedAgentNav.SetDestination(hit.point);
+                Debug.Log($"Set NavMesh destination for {selectedAgentType} to: {hit.point}");
+            }
+            else
+            {
+                Debug.LogWarning($"Selected agent does not have a NavMeshAgent component!");
+            }
+        }
+        else
+        {
+            Debug.Log("Raycast did not hit any ground surface.");
+        }
+    }
+    
 }
